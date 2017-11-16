471667cc97ef4261ab48cf05fea432ad6f330c48
Daniel Kindt <danieltkindt@gmail.com>
2017-11-16 12:43:44 -0700

    so far just renamed models/comments to models/contacts

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,54 +1,55 @@
-
 angular.module('contact',[])
 .controller('MainCtrl',[
   '$scope','$http',
   function($scope,$http) {
-	$scope.test = "Hello world!";
+	  $scope.test = "Hello world!";
 
+	  $scope.contacts = [
+	  ];
 
-	$scope.contacts = [
+	  $scope.addContacts = function() {
 		
-			
+		  if($scope.number === '') { return; }
+    
+      // Add the contact
+      console.log("In addContacts with "+$scope.name);
+      $scope.create({
+        name: $scope.name,
+        number: $scope.number,
+      });
+    
+      $scope.name = '';
+		  $scope.number = '';
 
-	];
+	  } //End of addContacts
 
-	$scope.addContacts = function() {
-		
-		//var newobj = {title:$scope.formContent,upvotes:0}
-		//$scope.comments.push(newobj);
-
-		if($scope.number === '') { return; }
-      		console.log("In addComment with "+$scope.name);
-      		$scope.create({
-        	name: $scope.name,
-        	number: $scope.number,
-      		});
-      		$scope.name = '';
-		$scope.number = '';
-
-	}
-
-
-	$scope.getAll = function() {
-    		return $http.get('/contacts').success(function(data){
-      			angular.copy(data, $scope.contacts);
-    		});	
-  	};
+	  $scope.getAll = function() {
+      return $http.get('/contacts').success(function(data){
+        angular.copy(data, $scope.contacts);
+      });	
+  	}; //End of getAll
   	$scope.getAll();
 
-	$scope.create = function(contact) {
-    		return $http.post('/contacts', contact).success(function(data){
-      			$scope.contacts.push(data);
-    		});
-  	};	
+	  $scope.create = function(contact) {
+      return $http.post('/contacts', contact).success(function(data){
+          $scope.contacts.push(data);
+        });
+    }; //End of create	
 
-	$scope.delete = function(contact) {
-      		$http.delete('/contacts/' + contact._id )
-        	.success(function(data){
-          		console.log("delete worked");
-        	});
-      		$scope.getAll();
-    	};
+	  $scope.delete = function(contact) {
+      $http.delete('/contacts/' + contact._id )
+        .success(function(data){
+          console.log("delete worked");
+        });
+      	$scope.getAll();
+      }; //End of delete
+  
+  /* Function to send an SMS message using custom API built by Dan Kindt. */
+  $scope.sendSMS = function() {
+    console.log("Attempting to send SMS to " + $scope.name);
+    
+  }
+
 
   }
 ]);
