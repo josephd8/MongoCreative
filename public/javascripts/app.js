252b334e3a08269414cbd565904f719992bce8cf
Daniel Kindt <danieltkindt@gmail.com>
2017-11-16 21:15:36 +0000

    more changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,90 +1,84 @@
-angular.module('contact',[])
-.controller('MainCtrl',[
-  '$scope','$http',
-  function($scope,$http) {
-	  $scope.test = "Hello world!";
+angular.module("contact",[])
+    .controller("MainCtrl",[
+        "$scope","$http",
+        function($scope,$http) {
+            $scope.test = "Hello world!";
+            $scope.contacts = [];
 
-	  $scope.contacts = [
-	  ];
+        $scope.addContacts = function() {
+            if($scope.number === "") { return; }
+            // Add the contact
+            console.log("In addContacts with "+$scope.name);
+            $scope.create({
+                name: $scope.name,
+                number: $scope.number
+            });
 
-	  $scope.addContacts = function() {
-		
-		  if($scope.number === '') { return; }
-    
-      // Add the contact
-      console.log("In addContacts with "+$scope.name);
-      $scope.create({
-        name: $scope.name,
-        number: $scope.number,
-      });
-    
-      $scope.name = '';
-		  $scope.number = '';
-
-	  } //End of addContacts
-
-	  $scope.getAll = function() {
-      return $http.get('/contacts').success(function(data){
-        angular.copy(data, $scope.contacts);
-      });	
-  	}; //End of getAll
-  	$scope.getAll();
-
-	  $scope.create = function(contact) {
-      return $http.post('/contacts', contact).success(function(data){
-          $scope.contacts.push(data);
-        });
-    }; //End of create	
-
-	  $scope.delete = function(contact) {
-      $http.delete('/contacts/' + contact._id )
-        .success(function(data){
-          console.log("delete worked");
-          $scope.alertType = "alert alert-success";
-          $scope.alertMSG = "Deleted " + contact.name + " from contacts";
-        });
-      	$scope.getAll();
-      }; //End of delete
-  
-  /* Function to send an SMS message using custom API built by Dan Kindt. */
-  $scope.sendSMS = function(contact) {
-    console.log("Attempting to send SMS to " + contact.name);
-    $http({
-      url: "https://www.searchit.today/send-sms",
-      method: "POST",
-      data: {
-        "fName": contact.name,
-        "pNum": contact.number,
-      },
-      headers: {
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      transformRequest: function(o) {
-        var str = [];
-        for(var p in o) {
-          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(o[p]));
+            $scope.name = "";
+            $scope.number = "";
         };
-        return str.join("&");
-      } //End of transformRequest
 
-    }).then(function successCallback(response) {
-      console.log("success! response: " + response.data);
-      var msg = "Successfully sent SMS to " + contact.name + " using " + 
-        contact.number + ". Response: " + response.data;
-      $scope.alertType = "alert alert-success";
-      $scope.alertMSG = msg;
+        $scope.getAll = function() {
+            return $http.get("/contacts").success(function(data){
+                angular.copy(data, $scope.contacts);
+            });
+        };
+        $scope.getAll();
 
-    }, function errorCallback(response) {
-      console.log("failed! statusText: " + response.statusText);
-      $scope.alertType = "alert alert-danger";
-      var errorMsg = "Unable to send SMS.  Status Text: ";
-      errorMsg += response.statusText;
-      $scope.alertMSG = errorMsg;
-    });
-  };
+        $scope.create = function(contact) {
+            return $http.post("/contacts", contact).success(function(data){
+                $scope.contacts.push(data);
+            });
+        };
 
+        $scope.delete = function(contact) {
+            $http.delete("/contacts/" + contact._id )
+                .success(function(data){
+                    console.log("delete worked");
+                    $scope.alertType = "alert alert-success";
+                    $scope.alertMSG = "Deleted " + contact.name + " from contacts";
+                });
+            $scope.getAll();
+        };
 
-  }
+        /* Function to send an SMS message using API built by Dan Kindt. */
+        $scope.sendSMS = function(contact) {
+            var name = contact.name;
+            var phoneNumber = contact.number;
+            console.log("Attempting to send SMS to " + name);
+            $http({
+                url: "https://www.searchit.today/send-sms",
+                method: "POST",
+                data: {
+                    "fName":    name,
+                    "pNum":     phoneNumber
+                },
+                headers: {
+                    "Accept": "application/json, text/plain, */*",
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                transformRequest: function (obj) {
+                    var str = [];
+                    for(var p in obj) {
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    };
+                    return str.join("&");
+                }
+
+            }).then(function successCallback(response) {
+                var data = response.data;
+                console.log("SMS sent response: " + data);
+                $scope.alertType = "alert alert-info";
+                $scope.alertMSG = data;
+
+            }, function errorCallback(response) {
+                console.log("failed! statusText: " + response.statusText);
+                $scope.alertType = "alert alert-danger";
+                var errorMsg = "Unable to send SMS.  Status Text: ";
+                errorMsg += response.statusText;
+                $scope.alertMSG = errorMsg;
+            });
+        };
+    }
 ]);
 
